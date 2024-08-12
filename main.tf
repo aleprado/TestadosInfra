0744f9e999ffccbe208e74392f61bdfc4d85f9ac
aleprado <ale.nesti@gmail.com>
2024-08-12 09:05:12 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,43 @@
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
   credentials = file(var.credentials_file)
 }
 
-resource "google_storage_bucket" "data_bucket" {
-  name     = var.bucket_name
+# Bucket para almacenar la función
+resource "google_storage_bucket" "function_bucket" {
+  name     = var.function_bucket_name
   location = var.region
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name, location]
+  }
 }
 
+# Bucket para almacenar los archivos que la función procesará
+resource "google_storage_bucket" "data_bucket" {
+  name     = var.data_bucket_name
+  location = var.region
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name, location]
+  }
+}
+
+# Subir el archivo ZIP de la función al bucket de funciones
 resource "google_storage_bucket_object" "upload_trigger" {
   name   = "function_trigger.zip"
-  bucket = google_storage_bucket.data_bucket.name
+  bucket = google_storage_bucket.function_bucket.name
   source = "${path.module}/function/function_trigger.zip"
 }
 
+# Crear la función de Cloud Functions
 resource "google_cloudfunctions_function" "csv_processor" {
   name                  = "csvProcessor"
   runtime               = "python310"
-  source_archive_bucket = google_storage_bucket.data_bucket.name
+  source_archive_bucket = google_storage_bucket.function_bucket.name
   source_archive_object = google_storage_bucket_object.upload_trigger.name
   entry_point           = "process_csv"
   environment_variables = {
