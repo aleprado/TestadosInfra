b4b80403209c4e27437abc8c14d21e577444712d
aleprado <ale.nesti@gmail.com>
2024-08-12 09:04:15 -0300

    Update main.py

--- function/main.py
+++ function/main.py
@@ -1,53 +1,50 @@
-import csv
-import os
-from google.cloud import firestore, storage
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+  credentials = file(var.credentials_file)
+}
 
-def detect_delimiter(sample: str) -> str:
-    """
-    Detect the delimiter used in the sample string.
-    """
-    if ';' in sample:
-        return ';'
-    elif ',' in sample:
-        return ','
-    else:
-        raise ValueError('Unknown delimiter')
+# Bucket para almacenar la función
+resource "google_storage_bucket" "function_bucket" {
+  name     = var.function_bucket_name
+  location = var.region
 
-def process_csv(data, context):
-    bucket_name = data['bucket']
-    file_name = data['name']
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name, location]
+  }
+}
 
-    # Initialize Firestore and Storage clients
-    firestore_client = firestore.Client()
-    storage_client = storage.Client()
+# Bucket para almacenar los archivos que la función procesará
+resource "google_storage_bucket" "data_bucket" {
+  name     = var.data_bucket_name
+  location = var.region
 
-    # Define the path for the file in Cloud Storage
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name, location]
+  }
+}
 
-    # Read the file content
-    content = blob.download_as_text().splitlines()
+# Subir el archivo ZIP de la función al bucket de funciones
+resource "google_storage_bucket_object" "upload_trigger" {
+  name   = "function_trigger.zip"
+  bucket = google_storage_bucket.function_bucket.name
+  source = "${path.module}/function/function_trigger.zip"
+}
 
-    # Detect the delimiter from the first line of the CSV
-    delimiter = detect_delimiter(content[0])
-
-    # Parse the CSV content with the detected delimiter
-    csv_reader = csv.DictReader(content, delimiter=delimiter)
-
-    # Extract file name without extension for sub-collection name
-    sub_collection_name = os.path.splitext(file_name)[0]
-
-    # Reference to Firestore document
-    doc_ref = firestore_client.collection('Rutas').document('Cliente 1').collection(sub_collection_name)
-
-    # Process each row in the CSV
-    for idx, row in enumerate(csv_reader):
-        # Ensure 'estado_actual' column is present and set to an empty string if not present
-        if 'estado_actual' not in row:
-            row['estado_actual'] = ''
-
-        # Create a new document with data from CSV row
-        doc_ref.document(str(idx)).set(row)
-
-    # Optionally: return the number of documents created
-    return len(content)
+# Crear la función de Cloud Functions
+resource "google_cloudfunctions_function" "csv_processor" {
+  name                  = "csvProcessor"
+  runtime               = "python310"
+  source_archive_bucket = google_storage_bucket.function_bucket.name
+  source_archive_object = google_storage_bucket_object.upload_trigger.name
+  entry_point           = "process_csv"
+  environment_variables = {
+    GOOGLE_RUNTIME = "python310"
+  }
+  event_trigger {
+    event_type = "google.storage.object.finalize"
+    resource   = google_storage_bucket.data_bucket.name
+  }
+}
