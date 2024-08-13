7d31ed50cdbc875c83510cbc1a39d8645b5846c3
aleprado <ale.nesti@gmail.com>
2024-08-13 20:14:22 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "data_bucket_name" {
-  description = "El nombre del bucket de Cloud Storage para datos creado"
-  value       = length(google_storage_bucket.data_bucket) > 0 ? google_storage_bucket.data_bucket[0].name : data.google_storage_bucket.existing_data_bucket.name
+output "export_bucket_name" {
+  description = "El nombre del bucket de Cloud Storage para las exportaciones creado"
+  value       = length(google_storage_bucket.export_bucket) > 0 ? google_storage_bucket.export_bucket[0].name : data.google_storage_bucket.existing_export_bucket.name
 }
 
 output "function_bucket_name" {
@@ -8,7 +8,7 @@ output "function_bucket_name" {
   value       = length(google_storage_bucket.function_bucket) > 0 ? google_storage_bucket.function_bucket[0].name : data.google_storage_bucket.existing_function_bucket.name
 }
 
-output "function_name" {
-  description = "El nombre de la función de Cloud Functions creada"
-  value       = google_cloudfunctions_function.csv_processor.name
+output "export_function_name" {
+  description = "El nombre de la función de Cloud Functions creada para exportar subcolecciones"
+  value       = google_cloudfunctions_function.export_csv.name
 }
