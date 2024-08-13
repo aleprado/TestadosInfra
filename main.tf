265c6bc546e41f329c633088d0663c56eaf40a8d
aleprado <ale.nesti@gmail.com>
2024-08-13 20:43:56 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,29 +51,20 @@ resource "google_cloudfunctions_function" "export_csv" {
   runtime               = "python310"
   source_archive_bucket = google_storage_bucket_object.upload_export_trigger.bucket
   source_archive_object = google_storage_bucket_object.upload_export_trigger.name
-  entry_point           = "export"
+  entry_point           = "export_subcollections"
   environment_variables = {
     EXPORT_BUCKET_NAME = length(google_storage_bucket.export_bucket) > 0 ? google_storage_bucket.export_bucket[0].name : data.google_storage_bucket.existing_export_bucket.name
+  }
+
+  event_trigger {
+    event_type = "providers/cloud.pubsub/eventTypes/topic.publish"
+    resource   = google_pubsub_topic.export_topic.id
   }
 }
 
 # Crear un Pub/Sub topic para disparar la función exportCSV
 resource "google_pubsub_topic" "export_topic" {
   name = "export-csv-topic"
-}
-
-# Vincular la función exportCSV con el topic de Pub/Sub
-resource "google_cloudfunctions_function_iam_member" "export_csv_invoker" {
-  project        = var.project_id
-  region         = var.region
-  cloud_function = google_cloudfunctions_function.export_csv.name
-  role           = "roles/cloudfunctions.invoker"
-  member         = "serviceAccount:${google_cloudfunctions_function.export_csv.service_account_email}"
-}
-
-resource "google_cloudfunctions_function_event_trigger" "export_csv_trigger" {
-  function_name = google_cloudfunctions_function.export_csv.name
-  trigger_topic = google_pubsub_topic.export_topic.id
 }
 
 # Crear una tarea de Cloud Scheduler para ejecutar la función cada día a las 00:00 horas
