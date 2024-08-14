03d939af8e5a0ca87d4b5670890da3e925105ece
aleprado <ale.nesti@gmail.com>
2024-08-13 22:45:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,10 +9,44 @@ data "google_storage_bucket" "existing_data_bucket" {
   name = var.data_bucket_name
 }
 
+# Detectar si el bucket de funciones ya existe
+data "google_storage_bucket" "existing_function_bucket" {
+  name = var.function_bucket_name
+}
+
+# Detectar si el bucket de exportación ya existe
+data "google_storage_bucket" "existing_export_bucket" {
+  name = var.export_bucket_name
+}
+
 # Crear el bucket de datos solo si no existe
 resource "google_storage_bucket" "data_bucket" {
   count    = data.google_storage_bucket.existing_data_bucket.id == null ? 1 : 0
   name     = var.data_bucket_name
+  location = var.region
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name, location]
+  }
+}
+
+# Crear el bucket de funciones solo si no existe
+resource "google_storage_bucket" "function_bucket" {
+  count    = data.google_storage_bucket.existing_function_bucket.id == null ? 1 : 0
+  name     = var.function_bucket_name
+  location = var.region
+
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [name, location]
+  }
+}
+
+# Crear el bucket de exportación solo si no existe
+resource "google_storage_bucket" "export_bucket" {
+  count    = data.google_storage_bucket.existing_export_bucket.id == null ? 1 : 0
+  name     = var.export_bucket_name
   location = var.region
 
   lifecycle {
@@ -26,6 +60,13 @@ resource "google_storage_bucket_object" "upload_csv_trigger" {
   name   = "function_trigger.zip"
   bucket = length(google_storage_bucket.function_bucket) > 0 ? google_storage_bucket.function_bucket[0].name : data.google_storage_bucket.existing_function_bucket.name
   source = "${path.module}/function/csv_processor/function_trigger.zip"
+}
+
+# Subir el archivo ZIP de la función Export Subcollections al bucket de funciones
+resource "google_storage_bucket_object" "upload_export_trigger" {
+  name   = "export_trigger.zip"
+  bucket = length(google_storage_bucket.function_bucket) > 0 ? google_storage_bucket.function_bucket[0].name : data.google_storage_bucket.existing_function_bucket.name
+  source = "${path.module}/function/export/export_trigger.zip"
 }
 
 # Crear la función de Cloud Functions para procesar CSVs
@@ -43,30 +84,6 @@ resource "google_cloudfunctions_function" "csv_processor" {
     event_type = "google.storage.object.finalize"
     resource   = google_storage_bucket.data_bucket[0].name
   }
-}
-
-# Detectar si el bucket de exportación ya existe
-data "google_storage_bucket" "existing_export_bucket" {
-  name = var.export_bucket_name
-}
-
-# Crear el bucket de exportación solo si no existe
-resource "google_storage_bucket" "export_bucket" {
-  count    = data.google_storage_bucket.existing_export_bucket.id == null ? 1 : 0
-  name     = var.export_bucket_name
-  location = var.region
-
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [name, location]
-  }
-}
-
-# Subir el archivo ZIP de la función Export Subcollections al bucket de funciones
-resource "google_storage_bucket_object" "upload_export_trigger" {
-  name   = "export_trigger.zip"
-  bucket = length(google_storage_bucket.function_bucket) > 0 ? google_storage_bucket.function_bucket[0].name : data.google_storage_bucket.existing_function_bucket.name
-  source = "${path.module}/function/export/export_trigger.zip"
 }
 
 # Crear la función de Cloud Functions para exportar subcolecciones
