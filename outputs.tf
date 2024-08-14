599da78b64c4062456c7a98c2cc42a7bffc63a97
aleprado <ale.nesti@gmail.com>
2024-08-13 22:35:51 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,24 @@
-output "export_bucket_name" {
-  description = "El nombre del bucket de Cloud Storage de exportación creado"
-  value       = length(google_storage_bucket.export_bucket) > 0 ? google_storage_bucket.export_bucket[0].name : data.google_storage_bucket.existing_export_bucket.name
+¿output "data_bucket_name" {
+  description = "El nombre del bucket de Cloud Storage para datos"
+  value       = google_storage_bucket.data_bucket[0].name
 }
 
 output "function_bucket_name" {
-  description = "El nombre del bucket de funciones creado"
-  value       = length(google_storage_bucket.function_bucket) > 0 ? google_storage_bucket.function_bucket[0].name : data.google_storage_bucket.existing_function_bucket.name
+  description = "El nombre del bucket de Cloud Storage para funciones"
+  value       = google_storage_bucket.function_bucket[0].name
 }
 
-output "csv_function_name" {
-  description = "El nombre de la función de Cloud Functions para procesar CSV creada"
+output "export_bucket_name" {
+  description = "El nombre del bucket de Cloud Storage para exportaciones"
+  value       = google_storage_bucket.export_bucket[0].name
+}
+
+output "csv_processor_function_name" {
+  description = "El nombre de la función de Cloud Functions creada para procesar CSV"
   value       = google_cloudfunctions_function.csv_processor.name
 }
 
 output "export_function_name" {
-  description = "El nombre de la función de Cloud Functions para exportar subcolecciones creada"
+  description = "El nombre de la función de Cloud Functions creada para exportar subcolecciones"
   value       = google_cloudfunctions_function.export_csv.name
 }
